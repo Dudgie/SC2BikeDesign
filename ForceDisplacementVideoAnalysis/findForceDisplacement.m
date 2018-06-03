258192eff866364626c66c133664019f6984417e
Dudgie <dudgeonb@gmail.com>
2018-06-03 10:19:45 +0100

    Added analysis to the video file

---
+++ ForceDisplacementVideoAnalysis/findForceDisplacement.m
@@ -0,0 +1,21 @@
+function findForceDisplacement(m)
+    % Get file to read
+    [dfile,dpath] = uigetfile('*.dat','Select Data File To Load');
+    dataFile = fullfile(dpath, dfile);
+    M = csvread(dataFile);
+    t = M(1,:);
+    z = M(2,:);
+    
+    v = (z(2:end)-z(1:end-1))./(t(2:end)-t(1:end-1));
+    tS = (t(1:end-1)+t(2:end))/2;
+    a = (v(2:end)-v(1:end-1))./(tS(2:end)-tS(1:end-1));
+    
+    F = m*9.81+m*a;
+    vS = (v(1:end-1)+v(2:end))/2;
+    data = scatter(vS,F);
+    hold on
+    xlabel('Velocity of the mass / ms^(-1)')
+    ylabel('Foce from the calf / N');
+    title('Calf force velocity characteristic');
+    legend(data,'Data')
+end
