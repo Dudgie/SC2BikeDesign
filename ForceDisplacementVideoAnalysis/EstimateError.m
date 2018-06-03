2146dd59443d1d11371211142a431538f6ce38fd
Dudgie <dudgeonb@gmail.com>
2018-06-03 18:44:51 +0100

    Added new estimate for accuracy

--- ForceDisplacementVideoAnalysis/EstimateError.m
+++ ForceDisplacementVideoAnalysis/EstimateError.m
@@ -6,7 +6,7 @@ close all
 N = 11;
 
 % Get file to read
-[ifile,ipath] = uigetfile({'*.png','*.jpg'},'Select Image to Analyse');
+[ifile,ipath] = uigetfile;
 imageFile = fullfile(ipath, ifile);
 A = imread(imageFile);
 
@@ -31,16 +31,61 @@ xMeasured = (x'-xB(1))*Scale;
 
 r = xMeasured-xReal;
 
+%{
 scatter(r,xReal);
 hold on
 plot([0,0],[xReal(1),xReal(end)]);
 xlabel('Error in the measurement')
 ylabel('location')
 title('Graph of the error in the distance measurement')
+%}
 
 sigma = (sum(r.^2)/N)^0.5;
-dE = norminv([0.025 0.975],0,sigma);
+average = (max(r)+min(r))/2;
+dE = norminv([0.025 0.975],average,sigma);
 disp(dE);
+
+p = polyfit(xReal,r,2);
+Cf = polyval(p,xReal);
+xMeasuredCor = xMeasured - Cf;
+rCor = xMeasuredCor - xReal;
+sigmaCor = (sum(rCor.^2)/N)^0.5;
+averageCor = (max(rCor)+min(rCor))/2;
+dECor = norminv([0.025 0.975],averageCor,sigmaCor);
+disp(dECor);
+
+f1 = figure;
+scatter(rCor,xReal);
+hold on
+plot([0,0],[xReal(1),xReal(end)]);
+xlabel('Error in the measurement')
+ylabel('location')
+title('Graph of the error in the distance measurement using a parabola')
+
+xRealTan = xReal-150;
+xM = (xB(2)+xB(1))/2;
+xShift = x'-xM;
+
+
+w = 400;
+thetaMax = atan(D/(2*w));
+thetah = thetaMax*xShift/(xB(2)-xM);
+h = w*tan(thetah);
+
+rTan = h-xRealTan;
+sigmaTan = (sum(rTan.^2)/N)^0.5;
+averageTan = (max(rTan)+min(rTan))/2;
+dETan = norminv([0.025 0.975],averageTan,sigmaTan);
+disp(dETan);
+
+f2 = figure;
+scatter(rTan,xRealTan);
+hold on
+plot([0,0],[xRealTan(1),xRealTan(end)]);
+xlabel('Error in the measurement')
+ylabel('location')
+title('Graph of the error in the distance measurement using tans')
+
 
 
 
