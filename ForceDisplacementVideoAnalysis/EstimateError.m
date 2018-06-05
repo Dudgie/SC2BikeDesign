4ecb0835fd56b109685ad640b6eb2bfbe75cba65
Dudgie <dudgeonb@gmail.com>
2018-06-05 19:41:29 +0100

    Added max force characteristic

--- ForceDisplacementVideoAnalysis/EstimateError.m
+++ ForceDisplacementVideoAnalysis/EstimateError.m
@@ -31,14 +31,14 @@ xMeasured = (x'-xB(1))*Scale;
 
 r = xMeasured-xReal;
 
-%{
+f1 = figure;
 scatter(r,xReal);
 hold on
 plot([0,0],[xReal(1),xReal(end)]);
 xlabel('Error in the measurement')
 ylabel('location')
 title('Graph of the error in the distance measurement')
-%}
+
 
 sigma = (sum(r.^2)/N)^0.5;
 average = (max(r)+min(r))/2;
@@ -54,20 +54,20 @@ averageCor = (max(rCor)+min(rCor))/2;
 dECor = norminv([0.025 0.975],averageCor,sigmaCor);
 disp(dECor);
 
-f1 = figure;
+f2 = figure;
 scatter(rCor,xReal);
 hold on
 plot([0,0],[xReal(1),xReal(end)]);
 xlabel('Error in the measurement')
 ylabel('location')
-title('Graph of the error in the distance measurement using a parabola')
+title('Graph of the error in the distance measurement correcting using a parabola')
 
 xRealTan = xReal-150;
 xM = (xB(2)+xB(1))/2;
 xShift = x'-xM;
 
 
-w = 400;
+w = 600;
 thetaMax = atan(D/(2*w));
 thetah = thetaMax*xShift/(xB(2)-xM);
 h = w*tan(thetah);
@@ -78,17 +78,30 @@ averageTan = (max(rTan)+min(rTan))/2;
 dETan = norminv([0.025 0.975],averageTan,sigmaTan);
 disp(dETan);
 
-f2 = figure;
+f3 = figure;
 scatter(rTan,xRealTan);
 hold on
 plot([0,0],[xRealTan(1),xRealTan(end)]);
 xlabel('Error in the measurement')
 ylabel('location')
-title('Graph of the error in the distance measurement using tans')
+title('Graph of the error in the distance measurement correcting for curvature')
 
 
+pTan = polyfit(h,rTan,2);
+CfTan = polyval(pTan,h);
+hCor = h - CfTan;
+rCorTan = hCor - xRealTan;
+sigmaCorTan = (sum(rCorTan.^2)/N)^0.5;
+averageCorTan = (max(rCorTan)+min(rCorTan))/2;
+dECorTan = norminv([0.025 0.975],averageCorTan,sigmaCorTan);
+disp(dECorTan);
 
 
-
-
+f4 = figure;
+scatter(rCorTan,xRealTan);
+hold on
+plot([0,0],[xRealTan(1),xRealTan(end)]);
+xlabel('Error in the measurement')
+ylabel('location')
+title('Graph of the error in the distance measurement using both methods')
 
